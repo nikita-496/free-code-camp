d6f2f64007bf48b74d8fdeda6d05eca6c5b2d24a
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2021-04-14 09:51:11 +0500

    the timer is running

--- FrontEnd/25 + 5 Clock/src/Control.js
+++ FrontEnd/25 + 5 Clock/src/Control.js
@@ -1,22 +1,60 @@
 import React, {Component} from "react"
 import Timer from "./Timer"
 
-//импорт font font awesome
-
 
 class Control extends Component {
     constructor(props) {
         super(props);
+        this.interval = undefined
         this.state = {
             countBreak: 5,
-            countSession: 25
+            countSession: 25,
+            clockCount: 25 * 60,
+            currentTimer: "Session",
+            isPlaying: false,
+            interval: undefined
         }
         this.increase = this.increase.bind(this);
         this.decrease = this.decrease.bind(this)
     }
+    //Запауск таймера 
+    handlePlayPause = () => {
+        const { isPlaying } = this.state;
 
-    
-    //Запушить на гитхаб. Коммит - добавлена функциональность приложения
+        if ( isPlaying) { //если тайме приостановлен
+            clearInterval(this.interval);
+            this.setState({
+                isPlaying: false  
+            }) 
+        }
+        else { // если запущен
+             this.interval = setInterval(()=> { const {clockCount} = this.state;
+             this.setState({
+                 clockCount: clockCount - 1
+             })
+            }, 1000);
+            this.setState({
+                isPlaying: true  
+            })
+        }
+    } 
+ 
+
+    //Прекращение работы тайимера - время вышло
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    //Перевод к формату мм::cc (время)
+    transformToTime = (count) => {
+        const minutes = Math.floor(count / 60);
+        let seconds = count % 60; 
+
+        seconds = seconds < 10 ? ('0' + seconds) : seconds;
+
+        return `${minutes}:${seconds}`
+    }
+
     increase (e) {
             if (e.target.id === "break-increment") { 
                 this.setState({
@@ -55,19 +93,19 @@ class Control extends Component {
     }
 
     render() {
-        const {countBreak,countSession} = this.state 
+        const {countBreak,countSession,clockCount,currentTimer } = this.state 
         return(
             <div className = "control">
 
                 <div className= "length-control">
-                    <div className = "breack"> {/*breakParent*/}
+                    <div className = "breack">
                         <div className = "label" id = "break-label">Break Length</div>
 
                         <button className ="btn-level" id = "break-decrement" onClick = {this.decrease}>-</button>
 
                         <span className = "valueBreak-length"id = "break-length">{countBreak}</span>
                         
-                        <button className ="btn-level" id = "break-increment" onClick = {this.increase}>+</button> {/*last-child*/}
+                        <button className ="btn-level" id = "break-increment" onClick = {this.increase}>+</button> 
                     </div>{/*breack*/}
                         
                     <div className = "session">
@@ -80,7 +118,7 @@ class Control extends Component {
                         <button className ="btn-level" id = "session-increment" onClick = {this.increase}>+</button>
                     </div> {/*session*/}
                 </div> {/*length-control*/}
-                <Timer session = {this.state.countSession}/>
+            <Timer clockCount = {clockCount} currentTimer = {currentTimer} fun = {this.transformToTime} fun2 = {this.handlePlayPause}/> 
             </div>
         )
     }
