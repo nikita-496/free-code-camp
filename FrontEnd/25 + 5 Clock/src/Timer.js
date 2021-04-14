8a91e6feab966bb1537313ab35ec551851feec75
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2021-04-14 09:52:13 +0500

    the timer is running

--- FrontEnd/25 + 5 Clock/src/Timer.js
+++ FrontEnd/25 + 5 Clock/src/Timer.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 
+//импорт font font awesome
+import {faPlayCircle} from "../node_modules/@fortawesome/free-regular-svg-icons/faPlayCircle";
+import {faPauseCircle} from "../node_modules/@fortawesome/free-regular-svg-icons/faPauseCircle";
+import { FontAwesomeIcon } from "../node_modules/@fortawesome/react-fontawesome"
+
 class Timer extends Component {
-    constructor(props){
-        super(props);
-    }
     render() {
-        const {session} = this.props;
+        const {clockCount,currentTimer} = this.props;
         return(
             <div className = "taimer-container">
-                <span id ="timer-label">Session</span>
-                <div id ="time-left">{session}</div>
+                <h3 id ="timer-label">{currentTimer}</h3>
+                <div className = "time_format" id ="time-left">{this.props.fun(clockCount)}</div>
+                <div id = "taimer-control">
+                    <FontAwesomeIcon className="start" icon ={faPlayCircle} onClick = {this.props.fun2}/>
+                    <FontAwesomeIcon className="stop" icon ={faPauseCircle} onClick = {this.props.fun2}/>
+                    <button className ="reset">Refresh</button>
+                </div>
             </div>
         )
     }
